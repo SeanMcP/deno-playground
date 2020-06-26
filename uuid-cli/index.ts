735a6bc4f6048fc9f12b57e427dade95c6153e9f
Sean McPherson <git@seanmcp.com>
2020-06-26 10:03:32 -0400

    feat: add aliases

--- uuid-cli/index.ts
+++ uuid-cli/index.ts
@@ -1,6 +1,8 @@
 import { parse, v4 } from "./deps.ts";
 
-const { count, array = false } = parse(Deno.args);
+const { count, array = false } = parse(Deno.args, {
+  alias: { array: "A", count: "C" },
+});
 
 function newHandler(args: { count?: string; array?: boolean }) {
   const count = args.count && typeof +args.count === "number" ? args.count : 1;
@@ -21,11 +23,10 @@ switch (Deno.args[0]) {
     if (Deno.args[1]) {
       console.log(v4.validate(Deno.args[1]));
     } else {
-      console.warn('Pass a uuid to validate')
+      console.warn("Pass a uuid to validate");
     }
     break;
   default: {
     console.log("Use commands `new` or `validate`");
   }
 }
-
