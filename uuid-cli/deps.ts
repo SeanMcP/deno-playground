729f1ec516a2e7e4135b68070ac20eab980332c9
Sean McPherson <git@seanmcp.com>
2020-06-26 09:34:12 -0400

    feat: add command support, validate

--- uuid-cli/deps.ts
+++ uuid-cli/deps.ts
@@ -1,1 +1,5 @@
+// UUID v4 util
 export { v4 } from "https://deno.land/std/uuid/mod.ts";
+
+// Read argument flags
+export { parse } from "https://deno.land/std/flags/mod.ts";
--- uuid-cli/index.ts
+++ uuid-cli/index.ts
@@ -1,8 +1,31 @@
-import { v4 } from "./deps.ts";
+import { parse, v4 } from "./deps.ts";
 
-const [first] = Deno.args
-const count = first && typeof +first === 'number' ? first : 1
+const { count, array = false } = parse(Deno.args);
 
-for (let i = 0; i < count; i++) {
-  console.log(v4.generate());
+function newHandler(args: { count?: string; array?: boolean }) {
+  const count = args.count && typeof +args.count === "number" ? args.count : 1;
+  const arr = [];
+
+  for (let i = 0; i < count; i++) {
+    args.array ? arr.push(v4.generate()) : console.log(v4.generate());
+  }
+
+  args.array && console.log(arr);
 }
+
+switch (Deno.args[0]) {
+  case "new":
+    newHandler({ count, array });
+    break;
+  case "validate":
+    if (Deno.args[1]) {
+      console.log(v4.validate(Deno.args[1]));
+    } else {
+      console.warn('Pass a uuid to validate')
+    }
+    break;
+  default: {
+    console.log("Use commands `new` or `validate`");
+  }
+}
+
